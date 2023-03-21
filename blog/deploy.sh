ea5856a920112d83cf96742bd8e3df5b7b437a43
kongzhenlong <kongzhenlong@hetao101.com>
2023-03-21 10:33:04 +0800

    $msg

---
+++ blog/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+printf "发布 Deploying updates to KL GitHub...\n"
+
+cp -r public/ ../docs
+
+msg="[修改] rebuilding site $(date)"
+if [ -n "$*" ]; then
+    msg="$*"
+fi
+
+git commit -am '$msg'
+git push
+echo "all done."
