e0d27e4c5b72fb4426a49815402ac39d7292c93c
kongzhenlong <kongzhenlong@hetao101.com>
2023-03-21 15:07:55 +0800

    [修改] rebuilding site Tue Mar 21 15:07:54 CST 2023

--- blog/deploy.sh
+++ blog/deploy.sh
@@ -4,13 +4,14 @@ set -e
 
 printf "发布 Deploying updates to KL GitHub...\n"
 hugo
-cp -r public/ ../docs
+cp -r ../blog/public/ docs
 
 msg="[修改] rebuilding site $(date)"
 if [ -n "$*" ]; then
     msg="$*"
 fi
-git add content/post/.
+git add ../blog/content/post/.
+git add ../docs/.
 git commit -am "$msg"
 git push
 echo "[发布完成] all done."
